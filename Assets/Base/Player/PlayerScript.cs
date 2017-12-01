013592298558bb6d8988fce49b73a9c1d8c94adf
Grégoire Joncour <greg.joncour@gmail.com>
2017-12-01 15:57:44 +0100

    script spider, player damage

---
+++ Assets/Base/Player/PlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : LifeScript {
+
+    public override void Damage(int d)
+    {
+        base.Damage(d);
+        if (Pv <= 0)
+        {
+            SceneManager.LoadScene("Base");
+        }
+    }
+}
--- Assets/Base/Scripts/SpiderScript.cs
+++ Assets/Base/Scripts/SpiderScript.cs
@@ -7,13 +7,16 @@ public class SpiderScript : LifeScript {
 
     public Transform Destination;
     public LifeBarScript LifeBar;
+    private Animator animator;
     private NavMeshAgent agent;
-
+    bool waitActive = false; //so wait function wouldn't be called many times per frame
+    private bool isAttack = false;
+    
     public override void Damage(int d)
     {
         base.Damage(d);
         LifeBar.Damage(d);
-        GetComponent<Animator>().SetInteger("Pv", Pv);
+        animator.SetInteger("Pv", Pv);
         if(Pv <= 0)
         {
             GetComponent<BoxCollider>().enabled = false;
@@ -26,12 +29,57 @@ public class SpiderScript : LifeScript {
     {
         // agent.destination = Destination.position;
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         //agent = GetComponent<NavMeshAgent>();
-        agent.destination = Destination.position;
+        if(!isAttack)
+            agent.destination = Destination.position;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.gameObject.GetComponent<LifeScript>())
+            {
+                if (!waitActive)
+                {
+                    other.gameObject.GetComponent<LifeScript>().Damage(3);
+                    StartCoroutine(Wait());
+                }
+            }
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isAttack = true;
+            animator.SetBool("isAttack", isAttack);
+            agent.isStopped = true;
+        }
+            
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isAttack = false;
+            animator.SetBool("isAttack", isAttack);
+            agent.isStopped = false;
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        waitActive = true;
+        yield return new WaitForSeconds(1.0f);
+        waitActive = false;
     }
 
     /*private void Start()
