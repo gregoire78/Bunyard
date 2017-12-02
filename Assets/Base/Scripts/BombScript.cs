5926f5acb9094ee87ee0c36e17d3faf811b6aed6
Grégoire Joncour <greg.joncour@gmail.com>
2017-12-03 00:53:13 +0100

    Bomber scripting

---
+++ Assets/Base/Scripts/BombScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour {
+
+    private LifeScript ObjectInRange;
+    public int timer;
+    public float ExplosionPower;
+    public int Damage;
+    bool timeExpected = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(Example());
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (timeExpected)
+        {
+            if (other.tag == "Player")
+            {
+                Rigidbody ObjectInRange = other.gameObject.GetComponent<Rigidbody>();
+                other.gameObject.GetComponent<LifeScript>().Damage(Damage);
+                ObjectInRange.AddForce(Vector3.Normalize(ObjectInRange.transform.position - transform.position) * ExplosionPower);
+                Destroy(gameObject);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(timeExpected)
+        {
+            //ObjectInRange.Damage(20);
+            //ObjectInRange.AddForce(Vector3.Normalize(ObjectInRange.transform.position - transform.position) * ExplosionPower);
+            
+        }
+	}
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(timer);
+        timeExpected = true;
+    }
+}
---
+++ Assets/Base/Scripts/BoxScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : LifeScript {
+
+    public BoxCollider activatedDoor;
+
+    public override void Damage(int d)
+    {
+        
+        base.Damage(d);
+        if (Pv <= 0)
+        {
+            if (gameObject.tag == "BoxWithIndice") { activatedDoor.GetComponent<BoxCollider>().enabled = true; }
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
